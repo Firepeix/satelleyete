4d12e3b27579a829acbb69e9176f4cfc38c51e26
Guilherme <ggideoni@gmail.com>
2020-10-03 13:49:31 -0300

    test

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('satellites','ApiController@satellite' );
+Route::get('satellites',function (Request $request) {
+    return "OI";
+} );
