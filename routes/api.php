f96db3ca74b4fed8e9452926b7912a929070fbc6
Guilherme <ggideoni@gmail.com>
2020-10-03 18:56:49 -0300

    return ids

--- routes/api.php
+++ routes/api.php
@@ -68,6 +68,44 @@ function convert($x,$y,$z) {
     return $local;
 }
 
+Route::get('getids',function (){
+
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://sscweb.gsfc.nasa.gov/WS/sscr/2/observatories/",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "GET",
+      CURLOPT_HTTPHEADER => array(
+        "Accept: application/json"
+      ),
+    ));
+    
+    $response = curl_exec($curl);
+    
+    curl_close($curl);
+
+    $data = json_decode($response);
+
+    $data = $data->Observatory[1];
+
+    $ids= [];
+
+    foreach ($data as $key => $value) {
+        $ids[] = new stdClass();
+
+        $ids[$key]->id = $value->Id;
+        $ids[$key]->name = $value->Name;
+    }
+
+    return response()->json($ids);
+}
+);
+
 
 Route::get('satellite/{id}',function ($id='cluster2') {
 
