4018d239bc1ce6bd4e4b4abf3567d890787f66a2
Guilherme <ggideoni@gmail.com>
2020-10-04 12:55:33 -0300

    mongodb????

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -169,7 +169,7 @@ class ApiController extends Controller
     
         $ids= [];
 
-        $client = new MongoDB\Client('mongodb+srv://root:root@cluster0.hhll7.mongodb.net/?retryWrites=true&w=majority');
+        $client = new \MongoDB\Client('mongodb+srv://root:root@cluster0.hhll7.mongodb.net/?retryWrites=true&w=majority');
 
         $sate = $client->selectDatabase('satelleyete');
 
